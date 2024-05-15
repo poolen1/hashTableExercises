6c98b320ae4b41580a4c82927a2ee2c7a92354ae
Nicholas Poole <nicholas.poole@everi.com>
2024-05-15 14:19:38 -0700

    Initial Commit
    Includes method to return intersection of 2 integer arrays.

---
+++ HashTableExercises/Intersection.cs
@@ -0,0 +1,27 @@
+﻿
+namespace HashTableExercises
+{
+    public class Hashtables
+    {
+        public static List<int> GetIntersection(int[] array1, int[] array2)
+        {
+            List<int> intersection = new List<int>();
+            Dictionary<int, bool> hashTable = new Dictionary<int, bool>();
+
+            foreach (int i in array1)
+            {
+                hashTable[i] = true;
+            }
+
+            foreach (int i in array2)
+            {
+                if (hashTable.ContainsKey(i))
+                {
+                    intersection.Add(i);
+                }
+            }
+
+            return intersection;
+        }
+    }
+}
---
+++ HashTableExercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using HashTableExercises;
+
+Console.WriteLine("Intersection of arrays: ");
+
+int[] array1 = { 1, 2, 3, 4, 5 };
+int[] array2 = { 0, 2, 4, 6, 8 };
+
+List<int> intersectionResult = Hashtables.GetIntersection(array1, array2);
+
+Console.WriteLine(String.Join(", ", intersectionResult));
+
+Console.Read();
