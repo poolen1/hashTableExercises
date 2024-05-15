b9b795f7f4bfd9edb531155dee9606cd4c54b649
Nicholas Poole <nicholas.poole@everi.com>
2024-05-15 15:44:28 -0700

    Ex 2, 3
    Return first duplicate value.
    Return missing letter.

--- HashTableExercises/Intersection.cs
+++ HashTableExercises/Intersection.cs
@@ -23,5 +23,51 @@ namespace HashTableExercises
 
             return intersection;
         }
+
+        public static string GetFirstDuplicate(string[] array1) 
+        { 
+            string result = string.Empty;
+            Dictionary<string, bool> hashTable = new Dictionary<string, bool>();
+
+            foreach (string s in array1)
+            {
+                if (hashTable.ContainsKey(s))
+                {
+                    return s;
+                }
+
+                hashTable[s] = true;
+            }
+
+            return result;
+        }
+
+        public static string GetMissingLetter(string sentence)
+        {
+            string result = string.Empty;
+            Dictionary<char, bool> sentenceDictionary = new Dictionary<char, bool>();
+
+            foreach (char c in sentence)
+            {
+                sentenceDictionary[c] = true;
+            }
+
+            for (int i=0; i<26; i++)
+            {
+                char letter = (Convert.ToChar(i + 97));
+
+                if (sentenceDictionary.ContainsKey(letter) == false)
+                    return letter.ToString();
+            }
+
+            return result;
+        }
+
+        public static string GetFirstNondupedLetter(string word)
+        {
+            string result = string.Empty;
+
+            return result;
+        }
     }
 }
--- HashTableExercises/Program.cs
+++ HashTableExercises/Program.cs
@@ -1,12 +1,46 @@
 ﻿using HashTableExercises;
 
+Console.WriteLine("===================================");
 Console.WriteLine("Intersection of arrays: ");
 
-int[] array1 = { 1, 2, 3, 4, 5 };
-int[] array2 = { 0, 2, 4, 6, 8 };
+int[] array1 = [1, 2, 3, 4, 5];
+int[] array2 = [0, 2, 4, 6, 8];
 
 List<int> intersectionResult = Hashtables.GetIntersection(array1, array2);
 
 Console.WriteLine(String.Join(", ", intersectionResult));
+Console.WriteLine("===================================");
+Console.WriteLine("First duplicate from string array: ");
 
+string[] stringArray = ["a", "b", "c", "d", "c", "e", "f"];
+string result = Hashtables.GetFirstDuplicate(stringArray);
+
+if (result == string.Empty)
+{
+    Console.WriteLine("No duplicate strings found.");
+}
+else
+{
+    Console.WriteLine("First duplicate: " + result);
+}
+
+Console.WriteLine("===================================");
+Console.WriteLine("Get missing letter: ");
+
+string sentence = "the quick brown box jumps over a lazy dog";
+result = Hashtables.GetMissingLetter(sentence);
+
+if (result == string.Empty)
+{
+    Console.WriteLine("All letters are included.");
+}
+else
+{
+    Console.WriteLine("Missing letter: " + result);
+}
+
+Console.WriteLine("===================================");
+Console.WriteLine("First non-duplicated letter: ");
+
+Console.WriteLine("===================================");
 Console.Read();
